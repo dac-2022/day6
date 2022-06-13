665ee8a5df7caba1b4dcb37d157065749837f62d
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-13 13:32:37 +0530

    9

---
+++ 9.js
@@ -0,0 +1,7 @@
+function applyColor() {
+  // let ref = document.querySelectorAll("h1");
+  // ref[1].style.color = "red";
+  // let ref = document.querySelector("h1");
+  // let ref1 = ref.nextElementSibling;
+  // ref1.style.color = "Green";
+}
